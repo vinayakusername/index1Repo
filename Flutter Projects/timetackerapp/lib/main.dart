4c75595dc8ecd7faa694abf4e9ebcf406cab5355
vinayakusername <gpraut88@gmail.com>
2020-12-15 16:42:13 +0530

    updated

--- Flutter Projects/timetackerapp/lib/main.dart
+++ Flutter Projects/timetackerapp/lib/main.dart
@@ -1,1 +1,22 @@
-import'material'
+import 'package:flutter/material.dart';
+import 'package:timetackerapp/app/sign_in/sign_in_page.dart';
+
+// This is the starting point of the execution of an application
+void main() => runApp(MyApp());
+
+//MyApp is the root widget of the Flutter Application
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp
+    (
+      debugShowCheckedModeBanner: false,
+      title: 'Time Tracker',
+      theme: ThemeData
+      (
+        primarySwatch: Colors.indigo
+      ),
+      home: SignInPage(),
+    );
+  }
+}
